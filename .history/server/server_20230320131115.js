0d2d43a07448e793f0a3efdefa5bbc7056c6b5a6
Saleh Kalouti <salehkalouti@gmail.com>
2023-03-20 13:12:50 +0200

    second commit

---
+++ .history/server/server_20230320131115.js
@@ -0,0 +1,18 @@
+const { urlencoded } = require("express");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const bodyParser = require("body-parser");
+const errorHandler = require("./middleware/errorHandler.js");
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+// app.use(express.urlencoded());
+// app.use(bodyParser.json());
+app.use(express.json());
+app.use("/api/contacts", require("./routes/contactRoutes.js"));
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server Listening on port ${port}`);
+});
