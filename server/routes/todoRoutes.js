3aa5e91d080b680444b68b4edfc10d6bb4268e40
Saleh Kalouti <salehkalouti@gmail.com>
2023-03-20 16:11:56 +0200

    created routeing for todo

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,2 +1,28 @@
 const express = require("express");
 const router = express.Router();
+
+router
+  .get("/", (req, res) => {
+    console.log("get all lists");
+    res.end("get all lists");
+  })
+  .post((req, res) => {
+    console.log("post list");
+    res.end("post a list");
+  });
+
+router
+  .get("/:id", (req, res) => {
+    console.log("get a list");
+    res.end("get a list");
+  })
+  .put((req, res) => {
+    console.log("put a list");
+    res.end("put a list");
+  })
+  .delete((req, res) => {
+    console.log("delete a list");
+    res.end("delete a list");
+  });
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const server = express();
 const port = process.env.PORT || 8081;
 
-server.use("/api/todo");
+server.use("/api/todoLists", require("./routes/todoRoutes"));
 
 server.listen(port, () => {
   console.log(`Server Listening on port: http://localhost:${port}`);
